c6d10817c08dbd9d8a912c199578512f83cbe614
Joshua Barrass <josh.barrass.work@gmail.com>
2021-01-15 23:35:13 +0000

    Improve the main function and add a phrase updater

--- wasm/snakeisdead/letterstest.go
+++ wasm/snakeisdead/letterstest.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
+	"syscall/js"
 
 	"github.com/joshbarrass/SnakeIsDead/pkg/letters"
 	"github.com/llgcode/draw2d/draw2dimg"
@@ -32,22 +34,44 @@ func getCells(text string, topLeft [2]float64) ([]*letters.Cell, error) {
 	return cells, nil
 }
 
-var cells, _ = getCells("SNAKE IS DEAD", [2]float64{20, 20})
-
-func testDraw(gc *draw2dimg.GraphicContext) bool {
-	// fill background
-	gc.SetFillColor(cells[0].DeathColors[0])
-	gc.MoveTo(0, 0)
-	gc.LineTo(0, 10000)
-	gc.LineTo(10000, 10000)
-	gc.LineTo(10000, 0)
-	gc.LineTo(0, 0)
-	gc.Fill()
-	// draw cell
-	for _, cell := range cells {
-		cell.Draw(gc)
+// getDrawingFunction returns a function for drawing the cells for a
+// phrase
+func getDrawingFunction(phrase string) (func(*draw2dimg.GraphicContext) bool, error) {
+	cells, err := getCells(phrase, [2]float64{20, 20})
+	if err != nil {
+		return nil, err
 	}
-	return true
+	return func(gc *draw2dimg.GraphicContext) bool {
+		// fill background
+		gc.SetFillColor(cells[0].DeathColors[0])
+		gc.MoveTo(0, 0)
+		gc.LineTo(0, 10000)
+		gc.LineTo(10000, 10000)
+		gc.LineTo(10000, 0)
+		gc.LineTo(0, 0)
+		gc.Fill()
+		// draw cell
+		for _, cell := range cells {
+			cell.Draw(gc)
+		}
+		return true
+	}, nil
+}
+
+// Updater is a struct responsible for storing the drawing function
+// and wrapping it, allowing it to be updated more easily
+type Updater struct {
+	Func func(*draw2dimg.GraphicContext) bool
+}
+
+// Draw wraps the drawing function
+func (updater *Updater) Draw(gc *draw2dimg.GraphicContext) bool {
+	return updater.Func(gc)
+}
+
+// SetFunc updates the drawing function
+func (updater *Updater) SetFunc(f func(*draw2dimg.GraphicContext) bool) {
+	updater.Func = f
 }
 
 func main() {
@@ -60,7 +84,34 @@ func main() {
 		log.Fatalf("Could not create canvas")
 	}
 
-	cvs.Start(30, testDraw)
+	drawFunc, err := getDrawingFunction("SNAKE IS DEAD")
+	if err != nil {
+		panic(fmt.Sprintf("failed to get drawing function: %s", err))
+	}
+	updater := Updater{
+		Func: drawFunc,
+	}
+
+	js.Global().Set("UpdatePhrase", js.FuncOf(
+		func(this js.Value, i []js.Value) interface{} {
+			if len(i) != 1 {
+				return map[string]interface{}{
+					"error": "wrong number of arguments",
+				}
+			}
+			phrase := i[0].String()
+			newFunc, err := getDrawingFunction(strings.ToUpper(phrase))
+			if err != nil {
+				return map[string]interface{}{
+					"error": err.Error(),
+				}
+			}
+			updater.Func = newFunc
+			return map[string]interface{}{}
+		},
+	))
+
+	cvs.Start(30, updater.Draw)
 
 	// channel is unused, so this prevents main from terminating and killing the WASM
 	<-c
